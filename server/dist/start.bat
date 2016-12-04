551cf620a02bdbef3ece5aada0dba68cd83548a0
Batman <mahcoisa@gmail.com>
2016-12-03 23:43:50 -0200

    Fix startup script

--- server/dist/start.bat
+++
@@ -1,3 +0,0 @@
-java -Dnet.sf.odinms.wzpath="dist/wz" -jar dist\maplebr2-0.0.1-SNAPSHOT.jar
-pause
-#-DDEBUG=2 
---
+++ server/start.bat
@@ -0,0 +1,2 @@
+java -Dnet.sf.odinms.wzpath="dist/wz" -jar dist/maplebr2-0.0.1-SNAPSHOT.jar
+pause
